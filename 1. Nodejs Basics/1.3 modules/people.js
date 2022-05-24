dfb6a1489c662248eaee093c4dcf49d79a1972d2
akshat-singh-dev <akshatsinghdelhi@gmail.com>
2022-05-24 10:20:48 +0530

    modules & require

---
+++ 1. Nodejs Basics/1.3 modules/people.js
@@ -0,0 +1,10 @@
+const people = ['yoshi', 'mario', 'luigi', 'peach'];
+const ages = [20, 25, 30, 35];
+
+console.log(people);
+
+module.exports = {
+    // persons: people
+    // people: people
+    people, ages
+};
