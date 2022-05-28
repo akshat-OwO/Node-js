563112e289589528dd4281939287726851cea830
akshat-singh-dev <akshatsinghdelhi@gmail.com>
2022-05-28 16:25:25 +0530

    What is middleware?

---
+++ 5. Middleware/app.js
@@ -0,0 +1,40 @@
+const express = require('express');
+
+// express app
+const app = express();
+
+// register view engine
+app.set('view engine', 'ejs');
+
+// listen for requests
+app.listen(3000);
+
+app.use((req, res) =>{
+    console.log('new request made: ');
+    console.log('host: ', req.hostname);
+    console.log('path: ', req.path);
+    console.log('method: ', req.method);
+});
+
+app.get('/', (req, res) =>{
+    const blogs = [
+        {title: 'Yoshi finds eggs', snippet: 'lorem ipsum dolor sit amet consectetur'},
+        {title: 'Mario finds stars', snippet: 'lorem ipsum dolor sit amet consectetur'},
+        {title: 'How to defeat bowser', snippet: 'lorem ipsum dolor sit amet consectetur'},
+    ];
+    // res.render('index', {title: 'Home', blogs: blogs});
+    res.render('index', {title: 'Home', blogs});
+});
+
+app.get('/about', (req, res) =>{
+    res.render('about', {title: 'About Us'});
+});
+
+app.get('/blogs/create', (req, res) =>{
+    res.render('create', {title: 'Create New Blog'});
+})
+
+// 404 page
+app.use((req, res) =>{
+    res.status(404).render('404', {title: '404'});
+});
