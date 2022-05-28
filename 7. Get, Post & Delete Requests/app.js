b196f5d8de0e0f55d295d63691dd1afdf858e873
akshat-singh-dev <akshatsinghdelhi@gmail.com>
2022-05-28 23:18:16 +0530

    POST Requests & route parameters

---
+++ 7. Get, Post & Delete Requests/app.js
@@ -0,0 +1,73 @@
+const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const Blog = require('./models/blog');
+
+// express app
+const app = express();
+
+// connect to mongodb
+const dbURI = 'mongodb+srv://netninja:test1234@blogapp.gwt1w.mongodb.net/?retryWrites=true&w=majority';
+mongoose.connect(dbURI)
+    .then((result) => app.listen(3000))
+    .catch((err) => console.log(err));
+
+// register view engine
+app.set('view engine', 'ejs');
+
+// middleware & static files
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan('dev'));
+
+// routes
+app.get('/', (req, res) =>{
+    res.redirect('/blogs');
+});
+
+app.get('/about', (req, res) =>{
+    res.render('about', {title: 'About Us'});
+});
+
+// blog routes
+app.get('/blogs', (req, res) =>{
+    Blog.find().sort({ createdAt: -1 })
+        .then((result) =>{
+            res.render('index', {title: 'All Blogs', blogs: result});
+        })
+        .catch((err) =>{
+            console.log(err);
+        });
+});
+
+app.post('/blogs', (req, res) =>{
+    const blog = new Blog(req.body);
+
+    blog.save()
+        .then((result) =>{
+            res.redirect('/blogs')
+        })
+        .catch((err) =>{
+            console.log(err);
+        });
+});
+
+app.get('/blogs/:id', (req, res) =>{
+    const id = req.params.id;
+    Blog.findById(id)
+        .then((result) =>{
+            res.render('details', {blog: result, title: 'Blog Details'});
+        })
+        .catch(err =>{
+            console.log(err);
+        });
+});
+
+app.get('/blogs/create', (req, res) =>{
+    res.render('create', {title: 'Create New Blog'});
+})
+
+// 404 page
+app.use((req, res) =>{
+    res.status(404).render('404', {title: '404'});
+});
